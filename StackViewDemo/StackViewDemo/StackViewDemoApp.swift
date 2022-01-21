a882332c609a03ec981461d5aac7010de9ba232a
Loyio <i@loyio.me>
2022-01-21 17:32:22 +0800

    add stack view project

---
+++ StackViewDemo/StackViewDemo/StackViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackViewDemoApp.swift
+//  StackViewDemo
+//
+//  Created by Loyio Hex on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct StackViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
