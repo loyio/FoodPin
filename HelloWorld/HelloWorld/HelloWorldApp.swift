23c95d454c1c15d236718b93d627d76e78a41c7a
Loyio <i@loyio.me>
2022-01-21 10:47:19 +0800

    test commit

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Loyio Hex on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
