cd04c3d6d27c49b7a2fda761c727df7a3b7931a1
Loyio <i@loyio.me>
2022-01-25 22:21:13 +0800

    add custom colors and swift extension

--- FoodPin/FoodPin/Extensions/UIColor+Ext.swift
+++ FoodPin/FoodPin/Extensions/UIColor+Ext.swift
@@ -5,4 +5,13 @@
 //  Created by Loyio Hex on 1/25/22.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        let redValue = CGFloat(red) / 255.0
+        let greenValue = CGFloat(green) / 255.0
+        let blueValue = CGFloat(blue) / 255.0
+        self.init(red: redValue, green: greenValue, blue: blueValue, alpha: 1.0)
+    }
+}
--- FoodPin/FoodPin/View/RestaurantDetailView.swift
+++ FoodPin/FoodPin/View/RestaurantDetailView.swift
@@ -80,6 +80,7 @@ struct RestaurantDetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView{
             RestaurantDetailView(restaurant: Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "G/F, 72 Po Hing Fong, Sheung Wan, Hong Kong", phone: "232-923423", description: "Searching for great breakfast eateries and coffee? This place is for you. We open at 6:30 every morning, and close at 9 PM. We offer espresso and espresso based drink, such as capuccino, cafe latte, piccolo and many more. Come over and enjoy a great meal.", image: "cafedeadend", isFavorite: true))
+                .environment(\.dynamicTypeSize, .xxxLarge)
         }
         .accentColor(.white)
         
